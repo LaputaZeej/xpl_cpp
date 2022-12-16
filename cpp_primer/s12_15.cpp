22afe2fa5c61c0d589219babd57e0fc4f0243813
xiangpl <xiangpengle@qq.com>
2022-12-16 10:22:15 +0800

    first blood

---
+++ cpp_primer/s12_15.cpp
@@ -0,0 +1,13 @@
+//
+// Created by 项鹏乐 on 2022/12/15.
+//
+
+#include <iostream>
+
+int main(){
+    using  namespace std;
+    std::cout << "hello";
+    cout << endl;
+    cout <<"cpp";
+    return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    const int n = 10;
+    int attr[n] = {1,2,3,4,5};
+    printf("%d ",attr[0]);
+    return 0;
+}
